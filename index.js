25a3bac96d9a2b0e77af2dce7946677a32866097
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-05-08 22:38:12 +0100

    click event on JS file

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
-let dataNum = document.querySelector(".digitBtn");
+let dataNum = document.querySelectorAll(".digitBtn");
 let dataOperator = document.querySelector(".operationBtn");
 let delOperator = document.querySelector(".deleteBtn");
 let clearAll = document.querySelector("#clear");
 let equalTo = document.querySelector("#equalTo");
 
 
-function getUpperVal(){
-	return document.querySelector("#upper-value").innerText;
-}
+dataNum.forEach(item => {
+	item.addEventListener("click", () => {
+		// console.log("ive been clicked");
+		document.querySelector("#current-value").innerText = dataNum;
+	});
+});
 
-function printUpperVal(value){
-	document.querySelector("#upper-value").innerText=value;
-}
 
-function getCurrentVal(){
-	return document.querySelector("#current-value").innerText;
-}
 
-function printCurrentVal(digitVal){
-	if (digitVal === '') return document.querySelector("#current-value").innerText = digitVal
-	else return document.querySelector("#current-value").innerText = digitVal;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getUpperVal(){
+// 	return document.querySelector("#upper-value").innerText;
+// }
+
+// function printUpperVal(value){
+// 	document.querySelector("#upper-value").innerText=value;
+// }
+
+// function getCurrentVal(){
+// 	return document.querySelector("#current-value").innerText;
+// }
+
+// function printCurrentVal(digitVal){
+// 	if (digitVal === '') return document.querySelector("#current-value").innerText = digitVal
+// 	else return document.querySelector("#current-value").innerText = digitVal;
+// }
 
 
 
