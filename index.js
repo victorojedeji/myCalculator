32958229a50de2e94ff9578f49acd828978a768c
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-05-08 08:34:29 +0100

    little JS functions added

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+function getUpperVal(){
+	return document.querySelector("#upper-value").innerText;
+}
+
+function printUpperVal(value){
+	document.querySelector("#upper-value").innerText=value;
+}
+
+function getCurrentVal(){
+	return document.querySelector("#current-value").innerText;
+}
+
+function printCurrentVal(){
+	return document.querySelector("#current-value").innerText;
+}
+
+
+
+
+
+
+
+
+
+
+
 const themeSwitch = document.querySelector('input');
 
 themeSwitch.addEventListener('change', () => {
