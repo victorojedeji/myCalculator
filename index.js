9afdc6c45b25f7ee62cf59e167914c6c4375a480
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-05-12 21:55:34 +0100

    update on functionality

--- index.js
+++ index.js
@@ -1,43 +1,82 @@
 // getting all html elements needed and binding it to a value
 let dataNum = document.querySelectorAll(".digitBtn");
-let dataOperator = document.querySelector(".operationBtn");
+let dataOperator = document.querySelectorAll(".operationBtn");
 let delOperator = document.querySelector(".deleteBtn");
 let clearAll = document.querySelector("#clear");
 let equalTo = document.querySelector("#equalTo");
-let currentOperand = document.querySelector(".current-operation").innerText;
+let currentOperand = document.querySelector("#current-value").innerText;
+let upperOperand = document.querySelector("#upper-value").innerText;
+let operator = document.querySelector("#operator").innerText;
 
 
-// click event for each digit button and to display value in current operand
+
+
 dataNum.forEach(item => {
-	item.addEventListener("click", function(digit) {
-		digit = this.innerText;
-		document.querySelector(".current-operation").innerText += digit;
+	item.addEventListener("click", function() {
+		let digit = this.innerText;
+		document.querySelector("#upper-value").innerText += digit;
 	});
 });
 
-function digitValue(){
-	 
+
+dataOperator.forEach(item => {
+	item.addEventListener("click", function() {
+		let oper = this.innerText;
+		document.querySelector("#operator").innerText += oper;
+		if (document.querySelector("#upper-value").innerText !== "") {
+			dataNum.forEach(item => {
+				item.addEventListener("click", function() {
+					let digit = this.innerText;
+					document.querySelector("#current-value").innerText += digit;
+				});
+			});
+		} 
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// click event for each digit button and to display value in current operand
+// function currentOperandArea(){
+// 	dataNum.forEach(item => {
+// 		item.addEventListener("click", function() {
+// 			let digit = this.innerText;
+// 			document.querySelector(".current-operation").innerText += digit;
+// 			let dig = document.querySelector(".current-operation").innerText;
+// 			console.log(Number(dig));
+// 		});
+// 	});
+// };
+
+
+// currentOperandArea()
+
+
+
+
+// clearing the operaton section
+function clearOps(){ 
+	clearAll.addEventListener("click", function(){
+		let clear = "";
+		document.querySelector(".current-operation").innerText = clear;
+	});
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+clearOps();
 
 
 
@@ -57,30 +96,6 @@ function digitValue(){
 // 	if (digitVal === '') return document.querySelector("#current-value").innerText = digitVal
 // 	else return document.querySelector("#current-value").innerText = digitVal;
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
